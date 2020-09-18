b96f95b8fe25179930e6b92c15f3a6f0c9d71fcd
serosc95 <oscardleon95@gmail.com>
2020-09-18 04:19:11 +0000

    errors

---
+++ errors.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * er - prints the value at the top of the stack
+ * @str: is the list to print
+ * @stack: number of the line to be executed.
+ * @fd: number of the line to be executed.
+ * @argv: number of the line to be executed.
+ * @numberl: number of the line to be executed.
+ */
+
+void er(char *str, stack_t *stack, FILE *fd, char **argv, unsigned int numberl)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n", numberl, argv[0]);
+	free(str);
+	freelist(&stack);
+	fclose(fd);
+	free(argv);
+	errors(str, stack, fd, argv, numberl);
+	exit(EXIT_FAILURE);
+}
